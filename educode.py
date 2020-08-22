833acead41def501d642e733501bf4b58e133f4e
Eduardo Castillo <eduardocf22@gmail.com>
2020-08-22 14:31:08 -0500

    updated

--- educode.py
+++ educode.py
@@ -1,15 +1,26 @@
 # Open a file
 
-file = open ("/home/ecastillo/Downloads/seaslug.txt", mode = "r")
+#file = open ("/home/ecastillo/Downloads/seaslug.txt", mode = "r")
 
 # print import
-print (file.read())
+#print (file.read())
 
 # check whether file is closed
-print (file.closed)
+#print (file.closed)
 
 # Close file
-file.close()
+#file.close()
 
 # check whether file is closed
-print (file.closed)
+#print (file.closed)
+
+import numpy as np
+
+file = "/home/ecastillo/Downloads/seaslug.txt"
+
+data = np.loadtxt ("/home/ecastillo/Downloads/seaslug.txt", delimiter='\t', dtype=str)
+
+print(data[0])
+
+
+
