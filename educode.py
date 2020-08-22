749ea8a1bf8904ebb3ccf86c603dd148f2e90321
Eduardo Castillo <eduardocf22@gmail.com>
2020-08-21 22:52:34 -0500

    udpated

--- educode.py
+++ educode.py
@@ -4,3 +4,12 @@ file = open ("/home/ecastillo/Downloads/seaslug.txt", mode = "r")
 # print import
 print (file.read())
 
+# check whether file is closed
+print (file.closed)
+
+# Close file
+file.close()
+
+# check whether file is closed
+print (file.closed)
+
