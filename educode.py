b9afea473099eaad151ba38be84c61f020793c15
Eduardo Castillo <eduardocf22@gmail.com>
2020-08-22 16:12:31 -0500

    updated

--- educode.py
+++ educode.py
@@ -20,9 +20,9 @@
 import numpy as np
 #import pandas as pd
 #import matplotlib.pyplot as plt
-import matplotlib
-import sys
-print (sys.path)
+import matplotlib as pyplot
+#import sys
+#print (sys.path)
 
 
 
