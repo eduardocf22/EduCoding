a82b5d5c0a8d4aa901ba1613e0ecd648223dd741
Eduardo Castillo <eduardocf22@gmail.com>
2020-08-22 23:49:25 -0500

    Updated

--- educode.py
+++ educode.py
@@ -26,17 +26,27 @@ import matplotlib.pyplot as plt
 
 
 
-file = "/home/ecastillo/Downloads/seaslug.txt"
+#file = "/home/ecastillo/Downloads/seaslug.txt"
 
-data = np.loadtxt ("/home/ecastillo/Downloads/seaslug.txt", delimiter='\t', dtype=str)
+#data = np.loadtxt ("/home/ecastillo/Downloads/seaslug.txt", delimiter='\t', dtype=str)
 
-print(data[0])
+#print(data[0])
 
-data_float = np.loadtxt("/home/ecastillo/Downloads/seaslug.txt", delimiter="\t", dtype=float, skiprows=1)
+#data_float = np.loadtxt("/home/ecastillo/Downloads/seaslug.txt", delimiter="\t", dtype=float, skiprows=1)
 
-print (data[10])
+#print (data[10])
 
-plt.scatter(data_float[:, 0], data_float[:, 1])
-plt.xlabel('time (min.)')
-plt.ylabel('percentage of larvae')
-plt.show()
+#plt.scatter(data_float[:, 0], data_float[:, 1])
+#plt.xlabel('time (min.)')
+#plt.ylabel('percentage of larvae')
+#plt.show()
+
+
+#data = np.genfromtxt('/home/ecastillo/Downloads/titanic.csv', delimiter=',', names=True, dtype=None)
+#print (data["Fare"])
+#print (data["Survived"])
+
+file = "/home/ecastillo/Downloads/titanic.csv"
+d= np.recfromcsv(file)
+print(d[:3])
+
