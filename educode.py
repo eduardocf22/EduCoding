20dc8fa900904a7e20d267d3befb52077ef9dae6
Eduardo Castillo <eduardocf22@gmail.com>
2020-08-23 19:01:35 -0500

    updqted

--- educode.py
+++ educode.py
@@ -46,14 +46,55 @@ import matplotlib.pyplot as plt
 #print (data["Fare"])
 #print (data["Survived"])
 
-file = "/home/ecastillo/Downloads/titanic.csv"
-d= np.recfromcsv(file)
-print(d[:3])
+#file = "/home/ecastillo/Downloads/titanic.csv"
+#d= np.recfromcsv(file)
+#print(d[:3])
 
 
-import pandas as pd
-filename= "winequality-red.csv"
-data= pd.read_csv(filename)
-data.head(n)
+#import pandas as pd
+#filename= "winequality-red.csv"
+#data= pd.read_csv(filename)
+#data.head()
+#data_array = data.values
+
+#*******************************
+
+
+# Open a file
+
+file = open ("/home/ecastillo/Downloads/titanic_corrupt.txt", mode = "r")
+
+# print import
+print (file.read())
+
+# check whether file is closed
+print (file.closed)
+
+# Close file
+file.close()
+
+# check whether file is closed
+print (file.closed)
+
+
+#******
+
+# Assign filename: file
+file = "/home/ecastillo/Downloads/titanic_corrupt.txt"
+
+
+# Import file: data
+data = pd.read_csv(file, sep="\t", comment= '#', na_values= ['Nothing'])
+
+# Print the head of the DataFrame
+print(data.head())
+
+# Plot 'Age' variable in a histogram
+pd.DataFrame.hist(data[['Age']])
+plt.xlabel('Age (years)')
+plt.ylabel('count')
+plt.show()
+
+
 
 
